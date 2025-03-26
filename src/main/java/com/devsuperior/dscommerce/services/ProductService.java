dee601ce7fe0cdbf6b61a5ed807a479a600d0e49
Yuri Bernardo <yuri-bernardo@live.com>
2025-03-26 16:24:51 -0300

    DELETE product

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,4 +31,43 @@ public class ProductService {
         Page<Product> result = repository.findAll(pageable);
         return result.map(x-> new ProductDTO(x));
     }
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+
+        Product entity = new Product();
+
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        entity = repository.save(entity);
+
+        return new ProductDTO(entity);
+    }
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO dto) {
+
+        Product entity = repository.getReferenceById(id);
+
+        copyDtoToEntity(dto, entity);
+
+        entity = repository.save(entity);
+
+        return new ProductDTO(entity);
+    }
+    @Transactional
+    public void delete(Long id){
+         repository.deleteById(id);
+    }
+
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+    }
+
+
 }
